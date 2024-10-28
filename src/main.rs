03839c4fdad64b898ebd0709f8a4a23ba3e56c8c
zedow <zerrefltd@gmail.com>
2024-10-28 10:53:56 +0100

    feat: add not found on unsupported path

--- src/main.rs
+++ src/main.rs
@@ -16,22 +16,31 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buff_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buff_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buff_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
 
-    let response = format!(
-        "{status_line}\r\n
-        Content-Length: {length}\r\n\r\n
-        {contents}"
-    );
+        let response = format!(
+            "{status_line}\r\n
+            Content-Length: {length}\r\n\r\n
+            {contents}"
+        );
 
-    stream.write_all(response.as_bytes()).unwrap();
-    println!("Request: {:#?}", http_request);
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\n
+            Content-Length: {length}\r\n\r\n
+            {contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
